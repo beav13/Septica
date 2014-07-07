d99dd3c183d64daa465e6eb981ac6ddd417a99f6
Brokenmill <moraru.vm@gmail.com>
2014-07-07 23:26:57 +0300

    add load queue members on Preload
    Renamed PreloadVlad to Preload. Added load queue members and accessing
    methods.

--- scripts/preload.js
+++ scripts/preload.js
@@ -2,84 +2,122 @@
 // will also go through json like deck.json to get the resource paths to 
 // load additional images, besides the ones passed in imgRes
 
-var PreloadVlad = (function(){
+var Preload = (function(){
 	var instance = null;
 
-	function Preload1() {		
+	function Preload1() {
+		var self = this;
+		this.jsonRes;
+		this.imgRes;
+		this.soundRes;
 
-		manifest = null;
+		this.jsonQueue;
+		this.imgQueue;
+		this.soundQueue;
 
 		this.loadResources = function(progressCallback, phaseCallback, jsonRes, imgRes, soundRes) {
-			this.jsonRes = jsonRes;
-			this.imgRes = imgRes;
-			this.soundRes = soundRes;
+			this.progressCallback = progressCallback;
+			this.phaseCallback = phaseCallback;
+			this.jsonRes = arguments[2];
+			this.imgRes = arguments[3];
+			this.soundRes = arguments[4];
 
+			// start loading JSON resources
 			this.loadJson();
-			// this.loadImg();
-			// this.loadSound();
 		}
 
 		this.loadJson = function() {
 			// load JSON resources
 			console.log("JSON files to load " + this.jsonRes);
-			manifest = this.jsonRes;
-			// new preload object
 
-			if (manifest) {
+			if (this.jsonRes) {
 				console.log("Start loading JSON resources");
-				queue = new createjs.LoadQueue(); 
-				queue.addEventListener("complete", onJsonComplete);
-				queue.addEventListener("fileload", onJsonFileLoad);
+				this.jsonQueue = new createjs.LoadQueue(); 
+				this.jsonQueue.addEventListener("fileload", onJsonFileLoad);
+				this.jsonQueue.addEventListener("complete", onJsonComplete);
 
-				queue.loadManifest(manifest);
+				this.jsonQueue.loadManifest(this.jsonRes);
 			}
 		}
 
 		this.loadImg = function() {
 			// load Image resources
 			console.log("Images to load " + this.imgRes);
-			manifest = this.imgRes;
 
-			if (manifest) {
+			if (this.imgRes) {
 				console.log("Start loading Image resources");
-				queue = new createjs.LoadQueue(); 
-				queue.addEventListener("complete", onImgComplete);
-				queue.addEventListener("fileload", onImgFileLoad);
-				queue.addEventListener("progress", onImgProgress);
+				this.imgQueue = new createjs.LoadQueue(); 
+				this.imgQueue.addEventListener("fileload", onImgFileLoad);
+				this.imgQueue.addEventListener("progress", onImgProgress);
+				this.imgQueue.addEventListener("complete", onImgComplete);
 
-				queue.loadManifest(manifest);
+				this.imgQueue.loadManifest(this.imgRes);
+			}
+		}
+
+		this.loadSound = function() {
+			console.log("Sounds to load " + this.soundRes);
+
+			if (this.soundRes) {
+				console.log("Start loading Sound resources");
+				queue = new createjs.LoadQueue();
+				queue.addEventListener("fileload", onSoundLoad);
+				queue.addEventListener("complete", onSoundComplete);
+
+				queue.loadManifest(this.soundRes);
+			}
+			this.phaseCallback();
+		}
+
+		this.addDeckImages = function(cards) {
+			// initialize image
+			if (this.imgRes == null) {
+				this.imgRes = [];
+			}
+			for (var i = 0; i < cards.length; i++) {
+				var card = cards[i];
+				this.imgRes.push({id:(card.alias + card.type) , src:card.imagePath});
+			}
+		}
+
+		this.getImages = function() {
+			return this.imgQueue;
+		}
+
+		function onJsonFileLoad(event) {
+			console.log("JSON file loaded");
+			// if loaded JSON is the deck, add the image resources to the image manifest
+			if (event.item.id === "deck") {
+				self.addDeckImages(event.result.cards);
 			}
 		}
 
 		function onJsonComplete(event) {
 			console.log("JSON queue load complete");
-			instance.loadImg();
-		}
-
-		function onJsonFileLoad(event) {
-			console.log("JSON file loaded");
-			if (event.item.id === "deck") {
-				manifest = [];
-				for (var i = 0; i < event.result.cards.length; i++) {
-					var card = event.result.cards[i];
-					if (instance.imgRes == null) {
-						instance.imgRes = [];
-					}
-					instance.imgRes.push({id:(card.alias + card.type) , src:card.imagePath});
-				}
-			}
+			// go on and load images
+			self.loadImg();
 		}
 
 		function onImgComplete(event) {
 			console.log("Image queue load complete");
+			// go on and load sound
+			self.loadSound();
 		}
 
 		function onImgFileLoad(event) {
-			console.log("Image file loaded" + event.item);
+			console.log("Image file loaded " + event.item);
 		}
 
 		function onImgProgress(event) {
 			console.log("Image load queue progress");
+		}
+
+		function onSoundFileLoad(event) {
+			console.log("Sound file loaded " + event.item);
+		}
+
+		function onSoundComplete(event) {
+			console.log("Sound queue load complete");
 		}
 
 	}
--- scripts/septica.js
+++ scripts/septica.js
@@ -1,15 +1,30 @@
 function Septica() {
+	var self = this;
 
-	this.start = function() {
+	this.stage;
+
+	this.start = function(canvasName) {
 		console.log("starting Septica");
 
+		this.stage = new createjs.Stage("stage");
+
 		// send resources to load to the preloader
-		var preloader = new PreloadVlad.getInstance();
+		var preloader = new Preload.getInstance();
 		preloader.loadResources(onLoadProgress, onPhaseComplete, [{id:"deck", src:"resources/deck.json"}]);
 	}
 
 	function onPhaseComplete() {
-
+		// var img = Preload.getInstance().getImages().getResult("AHearts");
+		var bmp = new createjs.Bitmap(Preload.getInstance().getImages().getResult("AHearts"));
+		var bmp2 = new createjs.Bitmap(Preload.getInstance().getImages().getResult("QHearts"));
+		var bmp3 = new createjs.Bitmap(Preload.getInstance().getImages().getResult("7Hearts"));
+		
+		self.stage.addChild(bmp);
+		self.stage.addChild(bmp2);
+		bmp2.x += 50;
+		self.stage.addChild(bmp3);
+		bmp3.x += 100;
+		self.stage.update();
 	}
 
 	function onLoadProgress() {
